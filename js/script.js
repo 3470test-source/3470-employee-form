ba6b293e1b783f109e70329af51b3b5731521a8a
vignesh <3470test@gmail.com>
2026-05-22 17:55:39 +0530

    5 step builder done .

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-        /* <!-- State & City  --> */
+        /* <!----- State & City  -----> */
 
 const state = document.getElementById("state");
 const city = document.getElementById("city");
@@ -40,3 +40,114 @@ state.addEventListener("change",function(){
 
 /* -------------------------------------------------------------------------------------------------------------------------------------------- */
 
+    /* <!----- MULTI STEP FORM PREVIOUS ,CONTINUE & FINAL SUBMIT -----> */
+    
+const formSteps = document.querySelectorAll(".form-step");
+
+const nextBtns = document.querySelectorAll(".next-btn");
+
+const prevBtns = document.querySelectorAll(".prev-btn");
+
+let currentStep = 0;
+
+/*--- NEXT BUTTON ---*/
+nextBtns.forEach((btn)=>{
+
+    btn.addEventListener("click",()=>{
+
+        formSteps[currentStep]
+        .classList.remove("active");
+
+        currentStep++;
+
+        formSteps[currentStep]
+        .classList.add("active");
+
+    });
+
+});
+
+
+/*--- PREVIOUS BUTTON ---*/
+prevBtns.forEach((btn)=>{
+
+    btn.addEventListener("click",()=>{
+
+        formSteps[currentStep]
+        .classList.remove("active");
+
+        currentStep--;
+
+        formSteps[currentStep]
+        .classList.add("active");
+
+    });
+
+});
+
+
+/*--- FINAL SUBMIT ---*/
+document.getElementById("multiStepForm").addEventListener("submit",(e)=>{
+
+    e.preventDefault();
+
+    alert(
+        "Employee Form Submitted Successfully"
+    );
+
+});
+
+/* ---------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+    /* <!----- CERTIFICATION SHOW/HIDE -----> */ 
+
+const certificationType = document.getElementById("certificationType");
+
+const certificateUploadSection = document.getElementById("certificateUploadSection");
+
+
+/*--- CERTIFICATION CHANGE ---*/
+certificationType.addEventListener("change",function(){
+
+    if(this.value === "Certified"){
+
+        certificateUploadSection
+        .style.display = "block";
+
+    }else{
+
+        certificateUploadSection
+        .style.display = "none";
+
+    }
+
+});
+
+
+/*--- EXPERIENCE SHOW/HIDE ---*/
+
+const experienceStatus = document.getElementById("experienceStatus");
+
+const experienceSection = document.getElementById("experienceSection");
+
+
+/*--- EXPERIENCE CHANGE ---*/
+
+experienceStatus.addEventListener("change",function(){
+
+    if(this.value === "Experienced"){
+
+        experienceSection
+        .style.display = "block";
+
+    }else{
+
+        experienceSection
+        .style.display = "none";
+
+    }
+
+});
+
+/* ------------------------------------------------------------------------------------------------------------------------------------------- */
